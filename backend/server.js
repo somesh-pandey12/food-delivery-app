38ab92ae52e2036e2945f525244a95c2317b36c2
Somesh Pandey <someshpandeycil@gmail.com>
2026-04-22 14:45:33 +0530

    feat: add JWT_SECRET and link user routes to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
@@ -15,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use('/api/user', userRoutes);
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
